d6e9d52cd505122c289be900194a2c000096d80a
Ana Paula Armendariz Pacheco <143620833+aparmendariz@users.noreply.github.com>
2023-12-07 09:37:27 +0100

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -15,10 +15,17 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-# Setting the sys path to make sphinx find the python modules in the parent directory:
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like here:
+
 import os
 import sys
-sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath('..'))
+
+# --  Extensions ---------------------------------------------------
 
 #Adding the autodoc extension to be able to extract documentation from docstrings:
 #Napoleon extension to parse Numpy docstrings 
@@ -35,10 +42,19 @@ extensions = [
 #To include the numpy docstrings in the init: 
 napoleon_include_init_with_doc = True
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+
+# intersphinx configuration
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/{.major}'.format(sys.version_info), None),
+    'sklearn': ('https://scikit-learn.org/stable/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
+    'statsmodels': ('https://www.statsmodels.org/stable/', None),
+}
